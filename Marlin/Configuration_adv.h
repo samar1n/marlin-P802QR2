de57a5978162f6fdf9c4f5a74ed5ceded4d6f3c8
Samarin <samarinaleksandr@gmail.com>
2019-04-13 02:08:04 +0300

    fans pins

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -237,14 +237,13 @@
  * Multiple extruders can be assigned to the same pin in which case
  * the fan will turn on when any selected extruder is above the threshold.
  */
-#define E0_AUTO_FAN_PIN -1
-#define E1_AUTO_FAN_PIN -1
-#define E2_AUTO_FAN_PIN -1
-#define E3_AUTO_FAN_PIN -1
-#define E4_AUTO_FAN_PIN -1
-#define CHAMBER_AUTO_FAN_PIN -1
-#define EXTRUDER_AUTO_FAN_TEMPERATURE 50
-#define EXTRUDER_AUTO_FAN_SPEED   255  // == full speed
+#define E0_AUTO_FAN_PIN 				FAN1_PIN
+#define E1_AUTO_FAN_PIN 				FAN1_PIN
+#define E2_AUTO_FAN_PIN 				FAN1_PIN
+#define E3_AUTO_FAN_PIN 				FAN1_PIN
+#define E4_AUTO_FAN_PIN 				FAN1_PIN
+#define EXTRUDER_AUTO_FAN_TEMPERATURE 	60
+#define EXTRUDER_AUTO_FAN_SPEED   		128
 
 /**
  * Part-Cooling Fan Multiplexer
